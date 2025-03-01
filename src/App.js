c303e90f24d36b552c173ee7f06a3bcaddffa3a3
Quang <qth9368@rit.edu>
2025-03-01 18:40:57 -0500

    more guns

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ import sevensixtwo from './assets/ammo/7.62x39.png';
 import fivefivesix from './assets/ammo/5.56x45.png';
 import fiveseventwoeight from './assets/ammo/5.7x28.png'
 import sevensixthree from './assets/ammo/7.63x25.png'
-import fourtyfiveacp from './assets/ammo/.45 acp.png'
-import fiftyae from './assets/ammo/.50 ae.png'
+import fourtyfiveacp from './assets/ammo/45 acp.png'
+import fiftyae from './assets/ammo/50 ae.png'
 
 // GUNS
 import glock from './assets/guns/glock17.png';
@@ -126,177 +126,197 @@ const initialGuns = {
       ammo: ['7.63x25mm'],
       image: mausercninesix
     },
-
+    {
+      id: 10,
+      name: 'Beretta 92FS',
+      description: 'Standard Italian military sidearm known for its durability and accuracy.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 11,
+      name: 'SIG Sauer P226',
+      description: 'Popular law enforcement sidearm with a proven track record.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 12,
+      name: 'CZ 75 SP-01',
+      description: 'Ergonomic design and reliable performance in competitive shooting.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 13,
+      name: 'Walther P99',
+      description: 'German semi-automatic pistol with a modern design and polymer frame.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 14,
+      name: 'Heckler & Koch USP Compact',
+      description: 'Compact variant of the USP series known for its reliability and performance.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 15,
+      name: 'Browning Hi-Power',
+      description: 'Legendary pistol with a high-capacity magazine and classic design.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 16,
+      name: 'Springfield XD',
+      description: 'Modern striker-fired pistol with excellent ergonomics.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 17,
+      name: 'Glock 19',
+      description: 'Compact version of the Glock series favored by law enforcement.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 18,
+      name: 'SIG Sauer P320',
+      description: 'Modular design with a reliable striker-fired mechanism.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 19,
+      name: 'Smith & Wesson Model 686',
+      description: 'Robust revolver known for its precision and reliability.',
+      ammo: ['.357 Magnum'],
+      image: unknown
+    },
   ],
 
   Rifles: [
     {
-      id: 2,
+      id: 16,
       name: 'AK-47',
       description: 'A popular military assault rifle.',
       ammo: ['7.62x39mm'],
       image: ak
     },
     {
+      id: 1,
+      name: 'M16A4',
+      description: 'Standard issue American military rifle.',
+      ammo: ['5.56x45mm NATO'],
+      image: unknown
+    },
+    {
+      id: 2,
+      name: 'AR-15',
+      description: 'Lightweight and versatile rifle popular among civilians.',
+      ammo: ['5.56x45mm NATO'],
+      image: unknown
+    },
+    {
       id: 3,
-      name: 'AK-50',
-      description: 'A custom .50 BMG AK platform.',
-      ammo: ['.50 BMG'],
-      image: akfifty
+      name: 'FN FAL',
+      description: 'Battle rifle widely used by NATO forces.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
     },
     {
       id: 4,
-      name: 'Century arms WASR-10',
-      description: 'Romanian AK variant imported by Century Arms.',
-      ammo: ['7.62x39mm'],
+      name: 'Heckler & Koch G3',
+      description: 'Reliable battle rifle known for its rugged design.',
+      ammo: ['7.62x51mm NATO'],
       image: unknown
     },
     {
       id: 5,
-      name: 'BCM Recce 16 MCMR AR-15',
-      description: 'A high-quality AR-15 from Bravo Company.',
+      name: 'Steyr AUG',
+      description: 'Innovative bullpup design rifle from Austria.',
       ammo: ['5.56x45mm NATO'],
       image: unknown
     },
     {
       id: 6,
-      name: 'Daniel Defense & Aero Precision parts AR-15',
-      description: 'AR-15 built with Daniel Defense and Aero Precision components.',
+      name: 'L85',
+      description: 'British bullpup assault rifle, also known as the SA80.',
       ammo: ['5.56x45mm NATO'],
       image: unknown
     },
     {
       id: 7,
-      name: 'Aero Precision & BCM parts AR-15',
-      description: 'Custom AR-15 using Aero Precision and BCM parts.',
+      name: 'HK416',
+      description: 'Modern assault rifle designed for enhanced reliability.',
       ammo: ['5.56x45mm NATO'],
       image: unknown
     },
     {
       id: 8,
-      name: 'F1 firearms AR-10 6.5 Creedmoor',
-      description: 'AR-10 platform chambered in 6.5 Creedmoor by F1 Firearms.',
-      ammo: ['6.5 Creedmoor'],
+      name: 'FN SCAR-H',
+      description: 'High-caliber variant of the SCAR series used by special forces.',
+      ammo: ['7.62x51mm NATO'],
       image: unknown
     },
     {
       id: 9,
-      name: 'KAC SR-25',
-      description: 'Knight\'s Armament semi-auto rifle in 7.62x51.',
-      ammo: ['7.62x51mm'],
-      image: unknown
-    },
-    {
-      id: 10,
-      name: 'Bergara B-14 Timber',
-      description: 'Bolt-action rifle in 6.5 Creedmoor with a classic wooden stock.',
-      ammo: ['6.5 Creedmoor'],
-      image: unknown
-    },
-    {
-      id: 11,
-      name: 'Bergara B-14 HMR',
-      description: 'Hunting and Match Rifle variant in 6.5 Creedmoor.',
-      ammo: ['6.5 Creedmoor'],
-      image: unknown
-    },
-    {
-      id: 12,
-      name: 'Mosin Nagant',
-      description: 'Historic Russian bolt-action rifle.',
-      ammo: ['7.62x54R'],
-      image: unknown
-    },
-    {
-      id: 13,
-      name: 'M1 Garand',
-      description: 'Iconic U.S. WWII-era semi-automatic rifle.',
-      ammo: ['.30-06'],
-      image: unknown
-    },
-    {
-      id: 14,
-      name: 'Springfield M1903',
-      description: 'Classic bolt-action rifle used by U.S. forces.',
-      ammo: ['.30-06'],
-      image: unknown
-    },
-    {
-      id: 15,
-      name: 'Mauser Kar98 kurt',
-      description: 'German WWII-era bolt-action rifle.',
-      ammo: ['7.92x57 Mauser'],
-      image: unknown
-    },
-    {
-      id: 16,
-      name: 'Barrett M82A1',
-      description: 'Famous .50 BMG anti-materiel rifle.',
-      ammo: ['.50 BMG'],
-      image: unknown
-    },
-    {
-      id: 17,
-      name: 'Norinco Type 56 AKM',
-      description: 'Chinese AKM variant in 7.62x39mm.',
-      ammo: ['7.62x39mm'],
-      image: unknown
-    },
-    {
-      id: 18,
-      name: 'SKS',
-      description: 'Soviet semi-auto carbine pre-dating the AK-47.',
-      ammo: ['7.62x39mm'],
-      image: unknown
-    },
-    {
-      id: 19,
-      name: 'HK MR556A1',
-      description: 'Heckler & Koch civilian version of the HK416 in 5.56x45.',
+      name: 'Tavor TAR-21',
+      description: 'Compact, bullpup rifle used by the Israeli military.',
       ammo: ['5.56x45mm NATO'],
       image: unknown
     },
     {
-      id: 20,
-      name: 'HK MR762A1',
-      description: 'Heckler & Koch civilian version of the HK417 in 7.62x51.',
-      ammo: ['7.62x51mm'],
+      id: 10,
+      name: 'FN SCAR-L',
+      description: 'Light variant of the SCAR series offering modularity and adaptability.',
+      ammo: ['5.56x45mm NATO'],
       image: unknown
     },
     {
-      id: 21,
-      name: 'Transferable AK and AR',
-      description: 'Full-auto capable AK and AR rifles (NFA registered).',
-      ammo: ['7.62x39mm', '5.56x45mm NATO'],
+      id: 11,
+      name: 'M14',
+      description: 'Legacy rifle that blends semi-automatic fire with high power.',
+      ammo: ['7.62x51mm NATO'],
       image: unknown
     },
     {
-      id: 22,
-      name: 'AKG-47',
-      description: 'A specialized AK variant from AKG.',
-      ammo: ['7.62x39mm'],
+      id: 12,
+      name: 'SR-25',
+      description: 'Designated marksman rifle known for precision shooting.',
+      ammo: ['7.62x51mm NATO'],
       image: unknown
     },
     {
-      id: 23,
-      name: 'Zastava ZPAP M70',
-      description: 'Serbian-made AK variant in 7.62x39mm.',
-      ammo: ['7.62x39mm'],
+      id: 13,
+      name: 'Barrett M82',
+      description: 'Anti-materiel rifle capable of long-range precision.',
+      ammo: ['.50 BMG'],
       image: unknown
     },
     {
-      id: 24,
-      name: 'CMMG Mutant Mk47',
-      description: 'AR/AK hybrid rifle from CMMG in 7.62x39mm.',
-      ammo: ['7.62x39mm'],
+      id: 14,
+      name: 'Steyr Scout',
+      description: 'Lightweight rifle optimized for scouting and sniping.',
+      ammo: ['5.56x45mm NATO'],
       image: unknown
     },
     {
-      id: 25,
-      name: 'Accuracy International AXMC',
-      description: 'Modular bolt-action rifle in .338 Lapua Magnum.',
-      ammo: ['.338 Lapua Magnum'],
+      id: 15,
+      name: 'Dragunov Sniper Rifle (SVD)',
+      description: 'Semi-automatic sniper rifle designed for precise long-range shooting.',
+      ammo: ['7.62x54mmR'],
       image: unknown
+    },
+    {
+      id: 17,
+      name: 'AK-50',
+      description: 'A custom .50 BMG AK platform.',
+      ammo: ['.50 BMG'],
+      image: akfifty
     },
   ],
 
@@ -326,6 +346,76 @@ const initialGuns = {
       id: 6,
       name: 'Saiga-12',
       description: 'AK-style shotgun chambered in 12 Gauge.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 7,
+      name: 'Benelli M4',
+      description: 'A semi-automatic shotgun used by military and law enforcement.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 8,
+      name: 'Mossberg 500',
+      description: 'A reliable pump-action shotgun with various configurations.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 9,
+      name: 'Remington 1100',
+      description: 'A semi-automatic shotgun known for its smooth action.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 10,
+      name: 'Browning Auto-5',
+      description: 'The first successful semi-automatic shotgun design.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 11,
+      name: 'Winchester SXP',
+      description: 'A modern pump-action shotgun with a fast cycling action.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 12,
+      name: 'Stoeger M3000',
+      description: 'A versatile semi-automatic shotgun designed for sport shooting.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 13,
+      name: 'Kel-Tec KSG-25',
+      description: 'A high-capacity version of the Kel-Tec KSG with dual magazine tubes.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 14,
+      name: 'Ithaca 37',
+      description: 'A classic pump-action shotgun known for its bottom-eject design.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 15,
+      name: 'Saiga-12K',
+      description: 'A variant of the Saiga-12 designed for compact use.',
+      ammo: ['12 Gauge'],
+      image: unknown
+    },
+    {
+      id: 16,
+      name: 'Benelli Super Black Eagle 3',
+      description: 'A high-performance semi-automatic shotgun favored by professionals.',
       ammo: ['12 Gauge'],
       image: unknown
     },
@@ -360,6 +450,76 @@ const initialGuns = {
       ammo: ['.45 ACP'],
       image: unknown
     },
+    {
+      id: 8,
+      name: 'UMP45',
+      description: 'A compact and versatile SMG known for its .45 ACP chambering.',
+      ammo: ['.45 ACP'],
+      image: unknown
+    },
+    {
+      id: 9,
+      name: 'MAC-10',
+      description: 'A compact and lightweight SMG with a high rate of fire.',
+      ammo: ['.45 ACP'],
+      image: unknown
+    },
+    {
+      id: 10,
+      name: 'PP-19 Bizon',
+      description: 'A Russian SMG featuring a helical magazine for increased capacity.',
+      ammo: ['9x18mm'],
+      image: unknown
+    },
+    {
+      id: 11,
+      name: 'Sterling L2A3',
+      description: 'A classic British submachine gun renowned for its reliability.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 12,
+      name: 'CZ Scorpion Evo 3',
+      description: 'A modern SMG offering high performance and ergonomics.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 13,
+      name: 'SIG MPX',
+      description: 'A modular SMG designed for precision and adaptability.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 14,
+      name: 'Uzi',
+      description: 'An iconic Israeli SMG known for its compact design and reliability.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 15,
+      name: 'Beretta M12',
+      description: 'An Italian SMG with a reputation for performance and durability.',
+      ammo: ['9mm'],
+      image: unknown
+    },
+    {
+      id: 16,
+      name: 'H&K MP7',
+      description: 'A futuristic PDW/SMG known for its lightweight design and high penetration.',
+      ammo: ['4.6x30mm NATO'],
+      image: unknown
+    },
+    {
+      id: 17,
+      name: 'Sten Mk II',
+      description: 'A historic British SMG with a simple, effective design.',
+      ammo: ['9mm'],
+      image: unknown
+    },
   ],
 
   Machineguns: [
@@ -369,6 +529,111 @@ const initialGuns = {
       description: 'A light machine gun used for sustained fire.',
       ammo: ['5.56x45mm NATO'],
       image: mtwofournine
+    },
+    {
+      id: 2,
+      name: 'M60',
+      description: 'A heavy machine gun used by the U.S. military since the 1950s.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
+    },
+    {
+      id: 3,
+      name: 'PKM',
+      description: 'A reliable Russian general-purpose machine gun.',
+      ammo: ['7.62x54mmR'],
+      image: unknown
+    },
+    {
+      id: 4,
+      name: 'M240',
+      description: 'A versatile medium machine gun favored by NATO forces.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
+    },
+    {
+      id: 5,
+      name: 'Browning M2',
+      description: 'A heavy machine gun known for its destructive power.',
+      ammo: ['.50 BMG'],
+      image: unknown
+    },
+    {
+      id: 6,
+      name: 'MG42',
+      description: 'A World War II-era machine gun renowned for its high rate of fire.',
+      ammo: ['7.92×57mm Mauser'],
+      image: unknown
+    },
+    {
+      id: 7,
+      name: 'M1919 Browning',
+      description: 'A .30 caliber machine gun that served in several wars.',
+      ammo: ['.30-06 Springfield'],
+      image: unknown
+    },
+    {
+      id: 8,
+      name: 'FN MAG',
+      description: 'A general-purpose machine gun known for its reliability and firepower.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
+    },
+    {
+      id: 9,
+      name: 'HK21',
+      description: 'A German-designed machine gun often used as a squad automatic weapon.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
+    },
+    {
+      id: 10,
+      name: 'Ultimax 100',
+      description: 'A light machine gun renowned for its controllability and accuracy.',
+      ammo: ['5.56x45mm NATO'],
+      image: unknown
+    },
+    {
+      id: 11,
+      name: 'PKP Pecheneg',
+      description: 'An updated version of the classic PK machine gun with modern features.',
+      ammo: ['7.62x54mmR'],
+      image: unknown
+    },
+    {
+      id: 12,
+      name: 'QJZ-171',
+      description: 'A Chinese heavy machine gun used by modern armed forces.',
+      ammo: ['12.7x108mm'],
+      image: unknown
+    },
+    {
+      id: 13,
+      name: 'RPK',
+      description: 'A light machine gun variant of the AK series, designed for squad support.',
+      ammo: ['7.62x39mm'],
+      image: unknown
+    },
+    {
+      id: 14,
+      name: 'MG3',
+      description: 'An improved version of the MG42 with enhanced features.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
+    },
+    {
+      id: 15,
+      name: 'Minigun',
+      description: 'A rotary machine gun known for its extremely high rate of fire.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
+    },
+    {
+      id: 16,
+      name: 'M134 Vulcan',
+      description: 'A lightweight rotary machine gun used on aircraft and ground vehicles.',
+      ammo: ['7.62x51mm NATO'],
+      image: unknown
     },
   ],
 };
